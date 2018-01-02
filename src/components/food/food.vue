18310c0322cf3a6bce7993bd6d224a0500acb9e5
KingJA <kingjavip@gmail.com>
2018-01-02 17:30:37 +0800

    add food page

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -25,24 +25,37 @@
         <split v-if="food.info"></split>
         <div class="info" v-if="food.info">
           <h1 class="title">商品信息</h1>
-          <p class="text" >{{food.info}}</p>
+          <p class="text">{{food.info}}</p>
         </div>
         <split></split>
         <div class="rating">
           <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
-          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :desc="desc" :only-content="onlyContent"
+                        :ratings="food.ratings" @toggle="toggleContent" @select="selectRating"></ratingselect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings&&food.ratings.length">
+              <li v-for="rating in food.ratings" class="rating-item border-1px"
+                  v-show="needShow(rating.rateType,rating.text)">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img :src="rating.avatar" width="12" height="12" class="avatar">
+                </div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
+                <p class="text">
+                  <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                </p>
+
+              </li>
+
+            </ul>
+            <div class="no-rating" v-show="!food.ratings||!food.ratings.length">暂时没有评论</div>
+
+          </div>
         </div>
       </div>
 
 
     </div>
-
 
 
   </transition>
@@ -51,17 +64,29 @@
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
+  import ratingselect from 'components/ratingselect/ratingselect'
   import cartcontrol from 'components/cartcontrol/cartcontrol'
   import split from 'components/split/split'
+  import {formatDate} from 'common/js/date.js'
   import Vue from 'vue'
 
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
   export default {
     components: {
-      cartcontrol, split
+      cartcontrol, split, ratingselect
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     props: {
@@ -71,12 +96,20 @@
     },
     mounted() {
       this.$nextTick(function () {
-        console.log(this.food);
       });
+    },
+    filters:{
+      formatDate(time) {
+        let date=new Date(time);
+        return formatDate(date,'yyyy-MM-dd hh:mm');
+      }
+
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.foodScroll) {
             this.foodScroll = new BScroll(this.$refs.food, {
@@ -97,12 +130,35 @@
         if (!this.food.count) {
           Vue.set(this.food, "count", 1);
         }
+      },
+      toggleContent() {
+        this.onlyContent = !this.onlyContent;
+        this.$nextTick(() => {
+          this.foodScroll.refresh();
+        });
+      },
+      selectRating(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.foodScroll.refresh();
+        });
+      },
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
       }
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .food
     position fixed
     left 0
@@ -110,7 +166,6 @@
     bottom 48px
     z-index 30
     width 100%
-    height 100%
     background #fff
     transform translate3d(0, 0, 0)
     &.move-enter-active, &.move-leave-active
@@ -196,4 +251,50 @@
         color rgb(77, 85, 93)
         font-weight 200
         margin-top 6px
+    .rating
+      padding-top 18px
+      .title
+        color rgb(7, 17, 27)
+        line-height 14px
+        margin-left 18px
+        font-size 14px
+      .rating-wrapper
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px(rgba(7, 17, 27, 0.1))
+          .user
+            position absolute
+            right 0
+            top 16px
+            .name
+              font-size 10px
+              line-height 12px
+              color rgb(147, 153, 159)
+              margin-right 6px
+            .avatar
+              border-radius 50%
+          .time
+            font-size 10px
+            line-height 12px
+            color rgb(147, 153, 159)
+            margin-bottom 6px
+          .text
+            font-size 12px
+            line-height 16px
+            color rgb(7, 17, 27)
+            .icon-thumb_up, .icon-thumb_down
+              font-size 12px
+              line-height 16px
+              margin-right 4px
+            .icon-thumb_up
+              color rgb(0, 160, 220)
+            .icon-thumb_down
+              color rgb(147, 153, 159)
+        .no-rating
+          padding: 16px 0
+          font-size 12px
+          color rgb(147, 153, 159)
+
 </style>
